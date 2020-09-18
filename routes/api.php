2a04d46d82f781dd5397fc3a72c9e2bdcd7ed5e3
Jack Segal <jackbsegal@gmail.com>
2020-09-18 10:26:08 +0100

    WIP

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,41 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::any('/pdf', function (Request $request) {
+	
+	// $design = $request->query('design');
+	$key = $request->input('key');
+	$firstname = $request->input('firstname');
+	$age = $request->input('age');
+	$school = $request->input('school');
+	$message = $request->input('message');
+
+	$dompdf = new Dompdf();
+
+	$options = $dompdf->getOptions();
+	$options->setIsRemoteEnabled(true);
+	$dompdf->setOptions($options);
+
+	//return view('postcard', compact('firstname', 'age', 'school', 'message'))->render();
+
+	$html = view('postcard', compact('firstname', 'age', 'school', 'message'))->render();
+	$dompdf->loadHtml($html);
+	
+	$dompdf->setPaper('A4', 'landscape');
+	$dompdf->render();
+	// $dompdf->stream();
+	$content = $dompdf->output();
+
+    try {
+
+        $filename = 'cop26-ecard-'. $key . 'pdf';
+        $filePath = 'digital-activist/'.$filename;
+
+        $response = Storage::disk('spaces')->put($filePath, $content, 'public');
+        return response()->json(['src'=> Storage::cloud()->url($filePath)]);
+
+    } catch (Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }	
+});
